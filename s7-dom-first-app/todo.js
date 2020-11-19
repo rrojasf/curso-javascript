6a1519b4c2d0b3242fec1e4720bfcab1aa737e4f
Robert Rojas <robert@bunnystudio.com>
2020-11-18 21:19:44 -0500

    Updating list elements

--- s7-dom-first-app/todo.js
+++ s7-dom-first-app/todo.js
@@ -1,30 +1,51 @@
-const todoList = [];
+const todoList = JSON.parse(localStorage.getItem('todos')) || [];
+
+const updateTodo = () => {
+  localStorage.setItem('todos', JSON.stringify(todoList));
+}
+// load the list of elements
+const render = () => {
+  console.log('load the last list');
+  const todoListElement = document.getElementById('todo-list');
+  const todoTemplate = todoList.map( item => '<li>' + item + '</li>');
+  todoListElement.innerHTML = todoTemplate.join('');
+
+  const todoListElements = document.querySelectorAll('#todo-list li');
+  todoListElements.forEach( (element, index) => {
+    element.addEventListener('click', () => {
+      // console.log(todoListElements, element, index);
+      // remove element from #todo-list ul
+      element.parentNode.removeChild(element);
+      // remove element from array
+      todoList.splice(index, 1);
+      // update todoList
+      updateTodo();
+      // paint the list
+      render();
+    });
+  });
+}
 
 window.onload = () => {
-    console.log('ToDo List!');
+  console.log('ToDo List!');
+  render();
 
-    const form = document.getElementById('todo-form');
+  const form = document.getElementById('todo-form');
 
-    form.onsubmit = (e) => {
-        e.preventDefault();
-        const todo = document.getElementById('todo');
-        const todoText = todo.value;
-        
-        todo.value = '';
+  form.onsubmit = (e) => {
+    e.preventDefault();
+    const todo = document.getElementById('todo');
+    const todoText = todo.value;
+    
+    todo.value = '';
 
-        todoList.push(todoText);
-        
-        console.log('valor digitado', todoText);
-        console.log('valor actual lista', todoList);
+    todoList.push(todoText);
+    // save to localStorage
+    updateTodo();
 
-        const todoListElement = document.getElementById('todo-list');
-
-        /* todoListElement.innerHTML = '';
-        for(let index = 0; index < todoList.length; index++) {
-            todoListElement.innerHTML += '<li>' + todoList[index] + '</li>';
-        } */
-
-        const todoTemplate = todoList.map( item => '<li>' + item + '</li>');
-        todoListElement.innerHTML = todoTemplate.join('');
-    }   
+    console.log('valor digitado', todoText);
+    console.log('valor actual lista', todoList);
+    
+    render();
+  }   
 }
